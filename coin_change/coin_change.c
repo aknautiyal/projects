4f819333f9b60ce86a95c78ce2634967547f7acf
Ankit Nautiyal <ankit.k.nautiyal@outlook.com>
2018-10-31 23:46:14 +0530

    Add the coin change solution

---
+++ coin_change/coin_change.c
@@ -0,0 +1,80 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct prefix_t;
+struct tree_t;
+
+#define N 100
+#define M 4
+struct prefix_t {
+	int val;
+	int index;
+	struct tree_t *subtree;
+};
+
+struct tree_t {
+	int val;
+	struct prefix_t *prefix;
+	int num_prefix;
+	int is_leaf;
+};
+
+int traverse_tree_for_prefix(struct tree_t *t, int prefix_index)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; i < prefix_index; i++) {
+		if (!t->prefix[i].subtree)
+			continue;
+		if (t->prefix[i].subtree->is_leaf) {
+			count++;
+		}
+		else {	
+			count+=traverse_tree_for_prefix(t->prefix[i].subtree, i + 1);  		
+		}
+	}
+	return count;
+}
+
+void main()
+{
+	struct tree_t tree[N];
+	int i,j;
+	int set[M] = {2,5,3,6};
+	int count = 0;
+
+	for (i = 0; i < N; i++) {
+		tree[i].val = i + 1;
+		tree[i].prefix = malloc(M * sizeof (struct prefix_t));
+		tree[i].num_prefix = M;
+		tree[i].is_leaf = 0;
+		for (j = 0; j < tree[i].num_prefix; j++) {
+			tree[i].prefix[j].val = set[j];		
+			tree[i].prefix[j].index = j;		
+			if (tree[i].val == tree[i].prefix[j].val) {
+				struct tree_t *subtree = malloc(sizeof (struct tree_t));
+				subtree->val = tree[i].val;
+				subtree->is_leaf = 1;
+				subtree->num_prefix = 0;
+				subtree->prefix = NULL;
+				tree[i].prefix[j].subtree = subtree;
+				continue;
+			}
+			if (tree[i].val > tree[i].prefix[j].val) {
+				int diff = tree[i].val - tree[i].prefix[j].val;
+				tree[i].prefix[j].subtree = &tree[diff - 1]; 			
+			}
+		}
+	}
+
+	count = traverse_tree_for_prefix(&tree[N-1], M);
+	printf("N = %d, count = %d\n", N, count);
+
+	/* free */
+	for (i = 0; i < N; i++) {
+		if (tree[i].prefix) {
+			free(tree[i].prefix);
+			tree[i].prefix = NULL;
+		}
+	}
+}
